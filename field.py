f658553c2ad5affa0781b41b81cb5452f371157b
Absolute-Value <ji.kei1220@gmail.com>
2023-05-27 19:05:09 +0900

    コメント

--- field.py
+++ field.py
@@ -1,7 +1,7 @@
 import pygame
 from define import *
 
-class Base():
+class Base(): # ベースクラス
     def __init__(self, x, y, width=BASE_WIDTH, height=BASE_HEIGHT):
         self.x = x
         self.y = y
@@ -11,16 +11,18 @@ class Base():
         self.dirt_height = height * 5
 
     def draw(self, screen, color=WHITE, dirt_color=BROWN):
+        # ベースの周りの土を描画
         pygame.draw.polygon(screen, dirt_color, [(self.x - self.dirt_width//2, self.y), # 左
                                             (self.x, self.y + self.dirt_height//2), # 下
                                             (self.x + self.dirt_width//2, self.y), # 右
                                             (self.x, self.y - self.dirt_height//2)]) # 上
+        # ベースを白で描画
         pygame.draw.polygon(screen, color, [(self.x - self.width//2, self.y), # 左
                                             (self.x, self.y + self.height//2), # 下
                                             (self.x + self.width//2, self.y), # 右
                                             (self.x, self.y - self.height//2)]) # 上
         
-class HomeBaseAndLine(Base):
+class HomeBaseAndLine(Base): # ホームベースとベースラインクラス
     def __init__(self, x, y, width=BASE_WIDTH, height=BASE_HEIGHT, bias=SCREEN_HEIGHT // 6):
         super().__init__(x, y, width, height)
         self.bias = bias
@@ -30,15 +32,20 @@ class HomeBaseAndLine(Base):
         self.batter_box_height = height * 3
         
     def draw(self, screen, color=WHITE, dirt_color=BROWN):
+        # ベースの周りの土を描画
         pygame.draw.ellipse(screen, dirt_color, (self.x - self.dirt_width//2, self.y - self.dirt_height//2, self.dirt_width, self.dirt_height))
-        # ベースラインの描画
+
+        # ベースラインを白で描画
         pygame.draw.line(screen, color, (self.x, self.y), (0, self.bias), width=3)
         pygame.draw.line(screen, color, (self.x, self.y), (SCREEN_WIDTH, self.bias), width=3)
 
+        # バッターボックスを土色で塗りつぶす
         pygame.draw.rect(screen, dirt_color, (self.x - self.width - self.batter_box_width, 
                                               self.y - self.batter_box_height//2 - self.height//2,
                                               self.batter_box_width * 3,
-                                              self.batter_box_height)) # バッターボックスを土色で塗りつぶす
+                                              self.batter_box_height)) 
+        
+        # バッターボックスの枠を白で描画
         pygame.draw.rect(screen, color, (self.x - self.width - self.batter_box_width, 
                                          self.y - self.batter_box_height//2 - self.height//2, 
                                          self.batter_box_width, 
@@ -47,13 +54,15 @@ class HomeBaseAndLine(Base):
                                          self.y - self.batter_box_height//2 - self.height//2,
                                          self.batter_box_width,
                                          self.batter_box_height), width=2) # 右のバッターボックス
+        
+        # ホームベースを白で描画
         pygame.draw.polygon(screen, color, [(self.x - self.width//2, self.y - self.height), # 左上
                                             (self.x - self.width//2, self.y - self.height//2), # 左下
                                             (self.x, self.y), # 下
                                             (self.x + self.width//2, self.y - self.height//2), # 右下
                                             (self.x + self.width//2, self.y - self.height)]) # 右上
         
-class PicherMound(Base):
+class PicherMound(Base): # ピッチャーマウンドクラス
     def __init__(self, x, y, width=BASE_WIDTH, height=BASE_HEIGHT//4):
         super().__init__(x, y, width, height)
         self.dirt_height = height * 16
@@ -62,7 +71,7 @@ class PicherMound(Base):
         pygame.draw.ellipse(screen, dirt_color, (self.x - self.dirt_width//2, self.y - self.dirt_height//2, self.dirt_width, self.dirt_height))
         pygame.draw.rect(screen, color, (self.x - self.width//2, self.y - self.height//2, self.width, self.height))
 
-class Field():
+class Field(): # フィールドクラス
     def __init__(self):
         self.bias = SCREEN_HEIGHT // 6
         self.picher_mound = PicherMound(SCREEN_WIDTH // 2, self.bias + SCREEN_HEIGHT // 3)
